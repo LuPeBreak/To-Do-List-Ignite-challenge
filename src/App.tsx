8490044fddf590c058a50bf2bc58cec6c8225f9a
LuPeBreak <lfbmrj15@gmail.com>
2023-03-27 15:25:48 -0300

    Estrutura inicial

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,22 @@ import styles from "./App.module.css";
 import logo from "./assets/logo.svg";
 
 function App() {
+  const [taskList, setTaskList] = useState([
+    {
+      title: "task",
+      checked: true,
+    },
+  ]);
+
   return (
     <div className={styles.app}>
-      
+      <header>
+        <img src={logo} alt="Logo to do" />
+      </header>
+      <main>
+        <div></div>
+        <div></div>
+      </main>
     </div>
   );
 }
