01447e799d1f3cc69414cf1f8e57a8049e9db177
LuPeBreak <lfbmrj15@gmail.com>
2023-03-27 15:15:53 -0300

    Inicio do projeto - limpado projeto vite - reset css basico - variaveis de cores

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+import styles from "./App.module.css";
+import logo from "./assets/logo.svg";
+
+function App() {
+  return (
+    <div className={styles.app}>
+      
+    </div>
+  );
+}
+
+export default App;
